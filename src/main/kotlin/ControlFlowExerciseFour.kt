cc333922a3a890115e745d10068f1e2f49b7f61b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-09 12:37:41 +0700

    Add: ControlFlowExerciseFour.kt

---
+++ src/main/kotlin/ControlFlowExerciseFour.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val words: List<String> = listOf(
+        "dinosaur",
+        "limousine",
+        "magazine",
+        "language"
+    )
+
+    for (word in words) {
+        if (word.startsWith("l")) {
+            println(word)
+        }
+    }
+}
