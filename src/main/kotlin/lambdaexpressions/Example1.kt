5c22c9c1b8d403065ab06e0c72e061df22382f0c
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-09 13:07:13 +0700

    Refactor: implements lambda expressions

--- src/main/kotlin/lambdaexpressions/Example1.kt
+++ src/main/kotlin/lambdaexpressions/Example1.kt
@@ -1,9 +1,5 @@
 package lambdaexpressions
 
-fun uppercaseString(str: String): String {
-    return str.uppercase()
-}
-
 fun main() {
-    println(uppercaseString("hello"))
+    println({ str: String -> str.uppercase() }("hello"))
 }
