d82f1392f873e1b6ae461b0803a9b3ec334f6cd3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-09 11:43:16 +0700

    Add: BasicTypesExercise.kt

---
+++ src/main/kotlin/BasicTypesExercise.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+
+    println("a: $a")
+    println("b: $b")
+    println("c: $c")
+    println("d: $d")
+    println("e: $e")
+    println("f: $f")
+}
