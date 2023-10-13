6c70a2cd47b738a1f382954283398f636de21acf
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-13 20:27:26 +0700

    Integrate: discord rich presence

---
+++ src/main/kotlin/exercises/Exercise8.kt
@@ -0,0 +1,25 @@
+package exercises
+
+fun quadraticFormula(a: Double, b: Double, c: Double): List<Double> {
+    val discriminant = b * b - 4 * a * c
+
+    if (discriminant < 0) {
+        // The quadratic equation has complex solutions
+        return emptyList()
+    }
+
+    val root1 = (-b + Math.sqrt(discriminant) / (2 * a))
+    val root2 = (-b - Math.sqrt(discriminant) / (2 * a))
+
+    return listOf(root1, root2)
+}
+
+fun main() {
+    val a: Double = 1.0
+    val b: Double = 2.0
+    val c: Double = 3.0
+
+    val roots = quadraticFormula(a, b, c)
+
+    println(roots)
+}
