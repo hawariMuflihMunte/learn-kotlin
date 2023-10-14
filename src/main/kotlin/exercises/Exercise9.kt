681f6bf9ac44405833b7b9deed3fe1dbd65c4457
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-14 09:10:22 +0700

    Add: exercises/Exercise9.kt - loveFun

---
+++ src/main/kotlin/exercises/Exercise9.kt
@@ -0,0 +1,13 @@
+package exercises
+
+// Opposites Attract
+fun loveFun(
+    flowerA: Int,
+    flowerB: Int
+): Boolean {
+    return (flowerA % 2 == 0 && flowerB % 2 != 0) || (flowerA % 2 != 0 && flowerB % 2 == 0)
+}
+
+fun main() {
+
+}
