d57252996480eeb75f489afe7ff45d95047adc81
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-20 17:43:37 +0700

    Add(Class): Variables

---
+++ src/main/kotlin/learn_kotlin_deep/introduction/Variables.kt
@@ -0,0 +1,12 @@
+package learn_kotlin_deep.introduction
+
+class Variables {
+    fun example() {
+        var a: String = "initial"
+        println(a)
+        val b: Int = 1
+        val c = 3
+
+        println("$b\n$c")
+    }
+}
---
+++ src/main/kotlin/learn_kotlin_deep/introduction/main.kt
@@ -0,0 +1,7 @@
+package learn_kotlin_deep.introduction
+
+import learn_kotlin_deep.introduction.Variables
+
+fun main() {
+    Variables().example()
+}
