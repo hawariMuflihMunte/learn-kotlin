2bf310670856e7ce717065c4ac609e16e71213f3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-21 09:59:17 +0700

    Add: exercises/Exercise36.kt - Movie-ticket price

---
+++ src/main/kotlin/exercises/Exercise37.kt
@@ -0,0 +1,20 @@
+package exercises
+
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    if (age in 61..100) return 20
+    if (age in 13..60) return if (isMonday) 25 else 30
+    if (age in 1..12) return 15
+    return -1
+}
