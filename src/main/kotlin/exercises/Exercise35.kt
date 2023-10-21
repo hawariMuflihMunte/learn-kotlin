d8f1d43cd487fdf34f65a4a6e6e10fd0034c2fcd
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-21 08:42:00 +0700

    Refactor: exercises/Exercise35.kt - Use function types and lambda expressions in Kotlin

--- src/main/kotlin/exercises/Exercise35.kt
+++ src/main/kotlin/exercises/Exercise35.kt
@@ -5,11 +5,35 @@ package exercises
 * */
 
 fun main() {
-    val trickFunction = trick
-    trick()
+    val coins: (Int) -> String = { quantity ->
+        "$quantity quarters"
+    }
+
+//    val cupcake: (Int) -> String = { quantity ->
+//        "Have a cupcake"
+//    }
+
+    val treatFunction = trickOrTreat(false, coins)
+    val trickFunction = trickOrTreat(true, null)
+    treatFunction()
     trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    return if (isTrick) {
+        trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        treat
+    }
 }
 
 val trick = {
     println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
 }
